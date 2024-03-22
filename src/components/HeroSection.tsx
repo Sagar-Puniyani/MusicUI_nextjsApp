4e48c356179ec041f1b4a5198931a132e41c3d88
Sagar <sagarpuniyani@gmail.com>
2024-03-22 16:17:44 +0530

    Update HeroSection component and tailwind.config.ts

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Link from "next/link";
 import { Spotlight } from "@/components/ui/Spotlight";
+import { Button } from "./ui/moving-border";
+
 
 const HeroSection = () => {
     return (
@@ -8,7 +10,7 @@ const HeroSection = () => {
             <div className="p-4 relative z-10 w-full text-center">
                 <Spotlight
                     className="-top-40 left-0 md:left-60 md:-top-20"
-                    fill="white"
+                    fill="blue"
                 />
                 <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
                     Master the art of music
@@ -21,7 +23,15 @@ const HeroSection = () => {
                 </p>
 
                 <div className="mt-4">
-                    <Link href={"/courses"}>Explore courses</Link>
+                    <Link href={"/courses"}>
+                    <Button
+                        borderRadius="1.75rem"
+                        className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+                        >
+
+                        Explore courses
+                        </Button>
+                        </Link>
                 </div>
             </div>
         </div>
